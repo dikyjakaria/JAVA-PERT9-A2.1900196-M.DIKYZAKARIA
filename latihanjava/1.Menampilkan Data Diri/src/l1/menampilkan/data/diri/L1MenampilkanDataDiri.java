553de63804fb1d49f3e3053bbd4bd3a930614220
dikyjakaria <Dikyzakar@gmail.com>
2019-11-22 21:24:37 +0700

    JAVA-PERT9-A2.1900196-M.DIKYZAKARIA

---
+++ latihanjava/1.Menampilkan Data Diri/src/l1/menampilkan/data/diri/L1MenampilkanDataDiri.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l1.menampilkan.data.diri;
+
+/**
+ * NAMA  : Diky Zakaria
+ * NIM   : A2.1900196
+ * Kelas : TI-1C
+ * 
+ * @author dikizakaria
+ */
+public class L1MenampilkanDataDiri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Biodata" + "\n" + "-----------------" );
+        System.out.println("NAMA\t\t         : Diki Zakaria");
+        System.out.println("ALAMAT\t\t         : Pamulihan ");
+        System.out.println("TEMPAT LAHIR\t\t : Sumedang ");
+        System.out.println("TANGGAL LAHIR\t\t : 12 April 2000");
+        System.out.println("--------------------------");
+        System.out.println("HOBI\t\t: olahraga");
+        System.out.println("MAKES\t\t: kue");
+        System.out.println("MIKES\t\t: kopi");
+    }
+    
+}
